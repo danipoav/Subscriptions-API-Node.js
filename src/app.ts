c2060cbc04c31362e79c8fd228bbd54164b78c10
danipoav <dapoav2002@gmail.com>
2025-04-08 22:49:41 +0200

    Installing cors policy

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,19 @@ import protectedRoutes from './routes/protectedRoutes'
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 connectDB();
 
 app.use(express.json());
+
+//Restricciones de CORS para que solo permita solicitudes de mi dominio
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 //Ruta Autentificación para generar un token.
 app.use('/api/auth', authRouter);
