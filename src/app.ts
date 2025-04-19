0d276210847c47da482d8aa4187f444d3c5176ba
danipoav <dapoav2002@gmail.com>
2025-04-20 00:40:35 +0200

    Changing cors url

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 
 //Restricciones de CORS para que solo permita solicitudes de mi dominio
 app.use(cors({
-  origin: 'http://subs-page.s3-website.eu-west-3.amazonaws.com/',
+  origin: 'http://subs-page.s3-website.eu-west-3.amazonaws.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
